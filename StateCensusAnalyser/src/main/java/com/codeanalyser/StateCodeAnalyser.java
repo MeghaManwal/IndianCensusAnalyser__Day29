2aa7461954cc5411e8935dfed1196a4903894efb
MeghaManwal <https://github.com/MeghaManwal/Basic-Calculator.git>
2021-05-24 11:08:12 +0530

    Added:To check Record for State Code CSV file

---
+++ StateCensusAnalyser/src/main/java/com/codeanalyser/StateCodeAnalyser.java
@@ -0,0 +1,38 @@
+package com.codeanalyser;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Iterator;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+
+public class StateCodeAnalyser {
+	
+	public static  int LoadIndianCodeData(String csvPath) throws IOException {
+		Reader reader;
+		reader = Files.newBufferedReader(Paths.get(csvPath));
+		
+		@SuppressWarnings({ "unchecked", "rawtypes" })
+		CsvToBean <IndianCodeCSV> csvToBean = new CsvToBeanBuilder(reader)
+                                             .withType(IndianCodeCSV.class)
+                                             .withIgnoreLeadingWhiteSpace(true)
+                                             .build();
+		Iterator<IndianCodeCSV> codeCSVIterator = csvToBean.iterator();;
+		int entries = 0;
+		
+		while(codeCSVIterator.hasNext()) {
+			entries++;
+			@SuppressWarnings("unused")
+			IndianCodeCSV codeData = codeCSVIterator.next();
+		}
+		System.out.println(entries);
+		return entries;
+	}
+	
+	public static void main(String[] args) throws IOException {
+		String csvPath = "C:\\Users\\Varsha Manwal\\Desktop\\BridgeLabz\\Fellowship\\Day29_IndianStatesCensusAnalyzer\\StateCensusAnalyser\\Data\\codeFile.csv";
+		LoadIndianCodeData(csvPath);
+	}
+}
